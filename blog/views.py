03c93824c0984969d48720a92fb1a6f0f489802d
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-08 22:13:57 -0300

    Adding view in views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render # HTML
+from django.http import HttpResponse
 
 # Create your views here.
+# HTTP Request ↔ HTTP Response.
+# MVT(MVC)
+def my_view(request):
+  print("my_view")
+  return HttpResponse("blog do app")
