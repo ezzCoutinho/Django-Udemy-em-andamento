91e83fad544f92d971596fc1e40b53f664c56fbe
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-08 22:15:04 -0300

    Adding view in viwes.py

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request): # É uma viewer, a viewer faz a ponte entre o usuário e o servidor
+  print("HOME")
+  return HttpResponse("home do app")
+
