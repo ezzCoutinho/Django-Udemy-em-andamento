a5a4b2cefee000c993802ca828616e2092440b77
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-09 22:09:21 -0300

    Updating views.py

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request): # É uma viewer, a viewer faz a ponte entre o usuário e o servidor
-  print("HOME")
-  return render(request, "home/home.html")
+  return render(request, "home/index.html")
 
+def base(request):
+  return render(request, "global/base.html")
