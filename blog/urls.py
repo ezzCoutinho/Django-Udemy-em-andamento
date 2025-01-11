8c5f173f93bca6b67b0d02b6292d22e7e53b9c30
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-10 23:13:35 -0300

    Adding name_space and urls dinamics

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from blog import views as blog_views 
 
+app_name = "blog" # É um name_space de urls
+
 #blog/
 urlpatterns = [   
-    path("", blog_views.my_view), # São url's.
-    path("exemplo/", blog_views.exemplo), # São url's.
+    path("", blog_views.my_view, name="blog"), # São url's.
+    path("exemplo/", blog_views.exemplo, name="exemplo"), # São url's.
 ]
