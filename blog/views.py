9c195f43dee989238cbdd81663a2510a3a9d1186
ezzCoutinho <lammerlopes@hotmail.com>
2025-01-13 22:09:58 -0300

    Updating views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render # HTML
 from blog.data import posts
-
+from typing import Any
+from django.http import Http404, HttpRequest
 
 # Create your views here.
 # HTTP Request ↔ HTTP Response.
@@ -8,18 +9,26 @@ from blog.data import posts
 def blog(request):
   contexto = {
               "text": "Estamos no blog.",
-              "title_blog": "BLOG - ",
+              "title": "BLOG - ",
               "posts": posts
               }
   return render(request, "blog/index.html", contexto)
  
-def post(request, id):
+def post(request: HttpRequest, post_id: int):
+  found_post: dict[str, Any] | None = None
+  for post in posts:
+    if post["id"] == post_id:
+      found_post = post
+      break
+  if found_post is None:
+  # Se o post não for encontrado, você pode retornar uma página 404
+    raise Http404("Post não encontrado")    
   contexto = {
-              "text": f"Estamos no id: {id}.",
-              "title_id": f"ID: {id}- ",
-              "posts": posts
+              "text": f"Estamos no id: {post_id}.",
+              "title": found_post["title"] + " - ",
+              "post": found_post
               }
-  return render(request, "blog/index.html", contexto) 
+  return render(request, "blog/post.html", contexto) 
                 
 def exemplo(request):
   contexto = {
